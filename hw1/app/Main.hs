656ceaf389775f7d4005fc55e35079eb9fb948d7
pnaranja <paul.naranja@gmail.com>
2015-12-07 12:39:30 -0800

    Put HW01 in a stack project

---
+++ hw1/app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import HW01
+
+main :: IO ()
+main = undefined
---
+++ hw1/src/HW01.hs
@@ -0,0 +1,46 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW01 where
+
+-- Exercise 1 -----------------------------------------
+
+-- Get the last digit from a number
+lastDigit :: Integer -> Integer
+lastDigit x         = mod x 10
+
+-- Drop the last digit from a number
+dropLastDigit :: Integer -> Integer
+dropLastDigit x     = div x 10
+
+-- Exercise 2 -----------------------------------------
+
+toRevDigits :: Integer -> [Integer]
+toRevDigits 0       = []
+toRevDigits x       = (lastDigit x) : toRevDigits (dropLastDigit x)
+
+-- Exercise 3 -----------------------------------------
+
+-- Double every second number in a list starting on the left.
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = undefined
+
+-- Exercise 4 -----------------------------------------
+
+-- Calculate the sum of all the digits in every Integer.
+sumDigits :: [Integer] -> Integer
+sumDigits = undefined
+
+
+-- Exercise 5 -----------------------------------------
+
+-- Validate a credit card number using the above functions.
+luhn :: Integer -> Bool
+luhn = undefined
+
+-- Exercise 6 -----------------------------------------
+
+-- Towers of Hanoi for three pegs
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi = undefined
