f59c5be7fecd0b30f63b3c2f1ab9ba9a55b3c778
pnaranja <paul.naranja@gmail.com>
2015-12-07 16:29:59 -0800

    Finished Luhn Algorithm

--- hw1/src/HW01.hs
+++ hw1/src/HW01.hs
@@ -6,33 +6,33 @@ import Data.Char
 
 -- Get the last digit from a number
 lastDigit :: Integer -> Integer
-lastDigit x         = mod x 10
+lastDigit x                     = mod x 10
 
 -- Drop the last digit from a number
 dropLastDigit :: Integer -> Integer
-dropLastDigit x     = div x 10
+dropLastDigit x                 = div x 10
 
 -- Exercise 2 -----------------------------------------
 
 toRevDigits :: Integer -> [Integer]
-toRevDigits 0       = []
-toRevDigits x       = (lastDigit x) : toRevDigits (dropLastDigit x)
+toRevDigits 0                   = []
+toRevDigits x                   = (lastDigit x) : toRevDigits (dropLastDigit x)
 
 --Create toDigits from scratch
 firstDigit :: Integer -> Integer
 firstDigit x
-    | (n:ns) <- (show x)       = toInteger (digitToInt n)
-    | otherwise                = 0
+    | (n:_) <- (show x)         = toInteger (digitToInt n)
+    | otherwise                 = 0
 
 dropFirstDigit :: Integer -> Integer
 dropFirstDigit x
-    | x < 10                        = (-1)
-    | (n:ns) <- (show x)            = read ns :: Integer
-    | otherwise                     = (-1)
+    | x < 10                    = (-1)
+    | (_:ns) <- (show x)        = read ns :: Integer
+    | otherwise                 = (-1)
 
 toDigits :: Integer -> [Integer]
-toDigits (-1)         = []
-toDigits x         = (firstDigit x) : toDigits (dropFirstDigit x)
+toDigits (-1)                   = []
+toDigits x                      = (firstDigit x) : toDigits (dropFirstDigit x)
 
 
 -- Exercise 3 -----------------------------------------
@@ -46,15 +46,16 @@ doubleEveryOther (x:y:xs)       = x: (y*2) : doubleEveryOther xs
 -- Exercise 4 -----------------------------------------
 
 -- Calculate the sum of all the digits in every Integer.
+-- foldl anonymous function will add all digits in the array from toDigits
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
-
+sumDigits []                    = 0
+sumDigits (x:xs)                = ((\a -> foldl (+) 0 a) (toDigits x)) + sumDigits xs 
 
 -- Exercise 5 -----------------------------------------
 
 -- Validate a credit card number using the above functions.
 luhn :: Integer -> Bool
-luhn = undefined
+luhn x = mod (sumDigits (doubleEveryOther (toRevDigits x))) 10 == 0
 
 -- Exercise 6 -----------------------------------------
 
