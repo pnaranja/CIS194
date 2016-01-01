d2dd5764c222fe599dfd6547a6d2112ab47ea576
pnaranja <paul.naranja@gmail.com>
2016-01-01 11:47:11 -0800

    Started hw2

---
+++ spring13/hw2/LogAnalysis.hs
@@ -0,0 +1,8 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+import Log
+
+-- Exercise 1
+-- Parses an individual line from the log file. For example,
+-- parseMessage "E 2 562 help help" == LogMessage (Error 2) 562 "help help"
+parseMessage :: String -> LogMessage
