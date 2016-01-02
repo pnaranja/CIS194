fd68344a667bd9ab0064f639a72d417d3fd84ac1
pnaranja <paul.naranja@gmail.com>
2016-01-02 14:44:54 -0800

    Changing hw2 as a stack project

--- spring13/hw2/src/LogAnalysis.hs
+++ spring13/hw2/src/LogAnalysis.hs
@@ -5,4 +5,5 @@ import Log
 -- Exercise 1
 -- Parses an individual line from the log file. For example,
 -- parseMessage "E 2 562 help help" == LogMessage (Error 2) 562 "help help"
-parseMessage :: String -> LogMessage
+-- parseMessage :: String -> LogMessage
+-- parseMessage [] = LogMessage Unknown "Unknown Message"
