95585b7f905e7de4a5af222dcfbadafa19d0aa60
pnaranja <paul.naranja@gmail.com>
2016-01-09 00:01:03 -0800

    Finished homework 2

--- spring13/hw2/src/LogAnalysis.hs
+++ spring13/hw2/src/LogAnalysis.hs
@@ -31,6 +31,7 @@ insert message@(LogMessage _ timestamp1 _) tree@(Node mtree1 tmessage@(LogMessag
     |  timestamp1>timestamp2     =   Node mtree1 tmessage (insert message mtree2) 
     |  timestamp1<timestamp2     =   Node (insert message mtree1) tmessage mtree2 
     |  otherwise                 =   tree
+insert _ (Node mtree1 (Unknown s) mtree2) =  Node mtree1 (Unknown s) mtree2 -- This should never happen since MessageTrees don't have Unknowns
 
 -- Exercise 3
 -- Build a Message Tree from a list of LogMessages
@@ -43,4 +44,20 @@ build mlist = foldr insert Leaf mlist
 inOrder :: MessageTree -> [LogMessage]
 inOrder Leaf = []
 inOrder (Node mtree1 message mtree2) = inOrder mtree1 ++ message : inOrder mtree2
+
+-- Exercise 5
+-- Extract errors with a severity of >= 50
+-- Take an UNSORTED list of Log Message and return a list of Strings
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong message = map getMessage (filter filterSevere message)
+
+filterSevere :: LogMessage -> Bool
+filterSevere (LogMessage (Error num) _ _)
+    |   num >= 50                           = True
+    |   otherwise                           = False
+filterSevere _                              = False
+
+getMessage :: LogMessage -> String
+getMessage (Unknown s) = s
+getMessage (LogMessage _ _ s) = s
 
