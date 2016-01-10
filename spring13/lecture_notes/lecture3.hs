b138e5a9ee18631f49b5f04389663e1a91e1b029
pnaranja <paul.naranja@gmail.com>
2016-01-10 10:15:33 -0800

    Starting lecture 3

---
+++ spring13/lecture_notes/lecture3.hs
@@ -0,0 +1,26 @@
+--Recursion Patterns
+
+data IntList = Empty | Cons Int IntList
+    deriving Show
+
+absAll :: IntList -> IntList
+absAll Empty = Empty
+absAll (Cons x xs) = Cons (abs x) (absAll xs)
+
+squareAll :: IntList -> IntList
+squareAll Empty = Empty
+squareAll (Cons x xs) = Cons (x*x) (squareAll xs)
+
+-- Extract the common things from the above two functions to create mapIntList function
+mapIntList :: (Int -> Int) -> IntList -> Int
+mapIntList f Empty = Empty
+mapIntList f (Cons x xs) = Cons (f x) (mapIntList xs)
+
+-- Let's Test!
+exampleList = Cons (-1) (Cons 2 (Cons (-6) Empty))
+
+addOne x = x+1
+squareIt x = x*x
+
+mapAddOneList = mapIntList addOne exampleList
+mapSquareItList = mapIntList squareIt exampleList
