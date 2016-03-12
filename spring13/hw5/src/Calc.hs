4f9b4c4f3bf459e2f1567f9327a0100d3359b436
pnaranja <paul.naranja@gmail.com>
2016-03-11 19:29:15 -0800

    First try at exercise 2

--- spring13/hw5/src/Calc.hs
+++ spring13/hw5/src/Calc.hs
@@ -1,8 +1,18 @@
 module Calc where
 import ExprT
+import Parser
+
+import Data.Maybe
 
 eval :: ExprT -> Integer
 eval (Lit x)    =  x
 eval (Add x y)  =  eval x + eval y
 eval (Mul x y)  =  eval x * eval y
             
+evalStr :: String -> Maybe Integer
+evalStr = handleMaybe . parseExp Lit Add Mul 
+
+handleMaybe :: Maybe a -> Maybe Integer
+handleMaybe (Just a) = Just (eval a)
+handleMaybe Nothing = Nothing
+
