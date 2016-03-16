af24101e91d45c6a7ad50ccb52dcb356e040b183
pnaranja <paul.naranja@gmail.com>
2016-03-16 00:20:21 -0700

    Finished Exercise 3

--- spring13/hw5/src/Calc.hs
+++ spring13/hw5/src/Calc.hs
@@ -17,7 +17,11 @@ evalStr x = eval <$> parseExp Lit Add Mul x
 
 -- Exercise 3
 class Expr a where
-    lit :: a -> a
+    lit :: Integer -> a
     add :: a -> a -> a
     mul :: a -> a -> a
 
+instance Expr ExprT where
+    lit = Lit 
+    add = Add 
+    mul = Mul 
