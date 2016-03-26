a01dab7d8108aa0a4d85174382c0c1f31183f470
pnaranja <paul.naranja@gmail.com>
2016-03-26 13:51:59 -0700

    Finished Exercise 2

--- spring13/hw6/src/Fibonacci.hs
+++ spring13/hw6/src/Fibonacci.hs
@@ -1,6 +1,8 @@
 module Fibonacci
     where
 
+-- Exercise 1
+-- Implement Fibonacci numbers
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
@@ -8,3 +10,16 @@ fib x = fib (x-1) + fib (x-2)
 
 fib1 :: [Integer]
 fib1 = map fib [0..]
+
+
+-- Exercise 2
+-- Create a faster way to implement Fibonacci numbers
+fib' :: Integer -> [Integer]
+fib' 0 = [0]
+fib' 1 = [0,1]
+fib' n = xs ++ [xs !! fromInteger (n-2) + xs !! fromInteger (n-1)]
+    where xs = fib' (n-1)
+
+fib2 :: [Integer]
+fib2 = map (last . fib') [0..]
+
