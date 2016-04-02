9e2ce61323e4dcc235b12b07c3cd6217da0bd918
pnaranja <paul.naranja@gmail.com>
2016-04-02 11:57:21 -0700

    Added more comments to try to solve ruler only using interleaving Streams

--- spring13/hw6/src/Fibonacci.hs
+++ spring13/hw6/src/Fibonacci.hs
@@ -55,13 +55,15 @@ nats  =  streamFromSeed (+1) 0
 -- Ruler function output: 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4, . . .
 -- nth element (starting with n=1), is largest power of 2 which evenly divides n 
 -- Example: n=1 1/2^0; n=2 2/2^1; n=3 3/2^0; n=4 4/2^2; n=5 5/2^0; n=6 6/2^1; n=7 7/2^0; n=8 8/2^3
--- Patterns are 0,1,0,2,0,1 and 0,3,0,4
+-- Patterns: 0, 1, 0, 2, 0, 1, 0 and 3,4,5...
+-- Patterns: 0's, 1,2,1,3,1,2,1,4... 
 ruler :: Stream Integer
 ruler = interLeaveStream interLeave012 interLeave03
     where
         interLeave012 = interLeaveStream (streamRepeat 0) (interLeaveStream (streamRepeat 1) (streamRepeat 2))
-        interLeave03 = interLeaveStream (streamRepeat 0) (streamRepeat 3)
+        interLeave03 = interLeaveStream (streamRepeat 0) (streamFromSeed (+1) 2)
 
 interLeaveStream :: Stream a -> Stream a -> Stream a
 interLeaveStream (Cons a a') (Cons b b') = Cons a (Cons b (interLeaveStream a' b'))
 
+test = Cons 0 (Cons 1 (Cons 0 (Cons 1 (Cons0))))
